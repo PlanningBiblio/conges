048cb85986fc7708e235293ea5d3004cda5db58e
Jérôme Combes <jerome@planningbiblio.fr>
2013-11-14 11:21:52 +0100

    Modification du numéro de version (1.3.7)

--- install.php
+++ install.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Planning Biblio, Plugin Congés Version 1.3.6
+Planning Biblio, Plugin Congés Version 1.3.7
 Licence GNU/GPL (version 2 et au dela)
 Voir les fichiers README.txt et COPYING.txt
 Copyright (C) 2013 - Jérôme Combes
@@ -23,7 +23,7 @@ if($_SESSION['login_id']!=1){
   exit;
 }
 
-$version="1.3.6";
+$version="1.3.7";
 include_once "../../include/config.php";
 
 $sql=array();
