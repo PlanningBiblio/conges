4e909149d664bb237e325fe114dbb2010413c4c0
Jérôme Combes <jerome@planningbiblio.fr>
2014-11-27 00:02:28 +0100

    Barre les congés dans le planning lors de la mise à jour des cellules en Ajax, suite à MAJ PlanningBiblio 1.8.6

---
+++ ajax.planning.updateCell.php
@@ -0,0 +1,70 @@
+<?php
+/*
+Planning Biblio, Plugin Conges Version 1.5.6
+Licence GNU/GPL (version 2 et au dela)
+Voir les fichiers README.md et LICENSE
+Copyright (C) 2013-2014 - Jérôme Combes
+
+Fichier : plugins/conges/ajax.planning.updateCell.php
+Création : 25 novembre 2014
+Dernière modification : 25 novembre 2014
+Auteur : Jérôme Combes, jerome@planningbilbio.fr
+
+Description :
+Fichier intégré au fichier planning/poste/ajax.updateCell.php permettant la mise à jour de la cellule modifiée
+Vérifie si les agents de la cellule modifiée sont en congés et les marques si c'est le cas
+*/
+
+/*
+Variables en entrée
+  $site,	$ajouter,	$perso_id,	$perso_id_origine,	$date
+  $debut,	$fin,		$absent,	$poste,			$barrer
+  $tab :
+    [0] => Array (
+      [nom] => Nom
+      [prenom] => Prénom
+      [statut] => Statut
+      [service] => Service
+      [perso_id] => 86
+      [absent] => 0
+      [supprime] => 0
+      )
+    [1] => Array (
+      ...
+
+Variable modifiée
+  $tab :
+    [0] => Array (
+      [nom] => Nom
+      [prenom] => Prénom
+      [statut] => Statut
+      [service] => Service
+      [perso_id] => 86
+      [absent] => 0
+      [supprime] => 0
+      [conges] => 0/1
+      )
+    [1] => Array (
+      ...
+*/
+$perso_ids=array();
+foreach($tab as $elem){
+  $perso_ids[]=$elem['perso_id'];
+}
+$perso_ids=join("','",$perso_ids);
+
+$db=new db();
+$db->select("conges","*","debut<'$date $fin' AND fin>'$date $debut' AND perso_id IN ('$perso_ids')");
+
+if($db->result){
+  for($i=0;$i<count($tab);$i++){
+    $tab[$i]['conges']=0;
+    foreach($db->result as $elem){
+      if($tab[$i]['perso_id']==$elem['perso_id']){
+	$tab[$i]['conges']=1;
+	continue;
+      }
+    }
+  }
+}
+?>
