b1be8e79d95d28b50886cb48e7ded547b83e352c
Jérôme Combes <jerome@planningbiblio.fr>
2015-01-13 11:52:55 +0100

    Suppression de la session "PLdate", MAJ PlanningBiblio 1.8.9

--- planning_cellule_poste.php
+++ planning_cellule_poste.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Planning Biblio, Plugin Conges Version 1.3
+Planning Biblio, Plugin Conges Version 1.5.9
 Licence GNU/GPL (version 2 et au dela)
 Voir les fichiers README.md et LICENSE
 Copyright (C) 2013-2015 - Jérôme Combes
 
 Fichier : plugins/conges/planning_cellule_poste.php
 Création : 30 janvier 2014
-Dernière modification : 30 janvier 2014
+Dernière modification : 12 janvier 2015
 Auteur : Jérôme Combes, jerome@planningbilbio.fr
 
 Description :
@@ -15,7 +15,6 @@ Fichier intégré au planning (planning/poste/fonctions.php)
 Vérifie si l'agent placé dans la cellule est en congés, si oui, il le barre en orange
 */
 
-$date=$_SESSION['PLdate'];
 foreach($GLOBALS['conges'] as $conge){
   if($conge['perso_id']==$elem['perso_id'] and $conge['debut']<"$date {$elem['fin']}" and $conge['fin']>"$date {$elem['debut']}"){
     $resultat="<s style='color:orange;'>$resultat</s>";
