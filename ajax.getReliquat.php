5ff48cd93045543bcb892e48164a9b395a207238
Jérôme Combes <jerome@planningbiblio.fr>
2015-03-18 12:42:06 +0100

    Initilisations de variables

--- ajax.getReliquat.php
+++ ajax.getReliquat.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Planning Biblio, Plugin Congés Version 1.4.5
+Planning Biblio, Plugin Congés Version 1.6.2
 Licence GNU/GPL (version 2 et au dela)
 Voir les fichiers README.md et LICENSE
 Copyright (C) 2013-2015 - Jérôme Combes
 
 Fichier : plugins/conges/ajax.getReliquat.php
 Création : 6 mars 2014
-Dernière modification : 10 mars 2014
+Dernière modification : 18 mars 2015
 Auteur : Jérôme Combes, jerome@planningbilbio.fr
 
 Description :
@@ -20,7 +20,7 @@ include "class.conges.php";
 $c=new conges();
 $c->perso_id=$_GET['perso_id'];
 $c->fetchCredit();
-$reliquatHeures=$c->elements['reliquat']?$c->elements['reliquat']:0;
+$reliquatHeures=array_key_exists("reliquat",$c->elements)?$c->elements['reliquat']:0;
 $reliquatJours=number_format($reliquatHeures/7,2,","," ");
 echo json_encode(array("reliquatHeures"=>$reliquatHeures,"reliquatJours"=>$reliquatJours));
 ?>
