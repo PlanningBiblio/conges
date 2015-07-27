51daee616b4d4e8d5b08a98870453fbdafd5c333
Jérôme Combes <jerome@planningbiblio.fr>
2015-07-27 08:21:03 +0200

    Corrige la mise à jour des cellules du planning. Les congés en attente de validation étaient barrés

--- ajax.planning.updateCell.php
+++ ajax.planning.updateCell.php
@@ -1,13 +1,13 @@
 <?php
 /*
-Planning Biblio, Plugin Conges Version 1.5.6
+Planning Biblio, Plugin Conges Version 2.0
 Licence GNU/GPL (version 2 et au dela)
 Voir les fichiers README.md et LICENSE
 Copyright (C) 2013-2015 - Jérôme Combes
 
 Fichier : plugins/conges/ajax.planning.updateCell.php
 Création : 25 novembre 2014
-Dernière modification : 25 novembre 2014
+Dernière modification : 27 juillet 2015
 Auteur : Jérôme Combes, jerome@planningbilbio.fr
 
 Description :
@@ -51,10 +51,10 @@ $perso_ids=array();
 foreach($tab as $elem){
   $perso_ids[]=$elem['perso_id'];
 }
-$perso_ids=join("','",$perso_ids);
+$perso_ids=join(",",$perso_ids);
 
 $db=new db();
-$db->select("conges","*","debut<'$date $fin' AND fin>'$date $debut' AND perso_id IN ('$perso_ids')");
+$db->select2("conges","*",array("debut"=>"<$date $fin", "fin"=>">$date $debut", "perso_id"=>"IN$perso_ids", "valide"=>">0"));
 
 if($db->result){
   for($i=0;$i<count($tab);$i++){
